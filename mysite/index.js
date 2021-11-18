856f98d38ad98050c5dcbcc984b304ea47f9c115
hanjoohyung <ghd167@naver.com>
2021-11-18 14:50:17 +0900

    router

--- mysite/index.js
+++ mysite/index.js
@@ -11,6 +11,7 @@ const { applicationRouter } = require('./routes');
 const { SIGTERM } = require('constants');
 
 // 3. Logger
+const logger = require('./logging');
 
 // 4. Application Setup
 const application = express()
@@ -29,16 +30,16 @@ applicationRouter.setup(application);
 // 6. Server Setup
 http.createServer(application)
     .on('listening', function(){
-        console.info(`http server runs on ${process.env.PORT}`);
+        logger.info(`http server runs on ${process.env.PORT}`);
     })
     .on('error', function(error){
         switch(error.code) {
             case 'EACCESS':
-                console.error(`${process.env.PORT} requires privileges`);
+                logger.error(`${process.env.PORT} requires privileges`);
                 process.exit(1);
                 break;
             case 'EADDRINUSE':
-                console.error(`${process.env.PORT} is already in use`);
+                logger.error(`${process.env.PORT} is already in use`);
                 process.exit(1);
                 break;
             default:
