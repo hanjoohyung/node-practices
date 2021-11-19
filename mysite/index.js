f5267d5f39debedd9055cb9d4daeb8a31a8118dd
hanjoohyung <ghd167@naver.com>
2021-11-19 13:58:10 +0900

    node mysite

--- mysite/index.js
+++ mysite/index.js
@@ -1,7 +1,9 @@
 const http = require('http');
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
 const dotenv = require('dotenv');
+const multer = require('multer');
 
 // 1. Environment Variables
 dotenv.config({path: path.join(__dirname, 'config/app.env') });
@@ -16,12 +18,21 @@ const logger = require('./logging');
 
 // 4. Application Setup
 const application = express()
-    // 4-1. static resources
-    .use(express.static(path.join(__dirname, process.env.STATIC_RESOURCES_DIRECTORY)))
+    // 4-1. Session Environment
+    .use(session({
+        secret: "mysite-session",
+        resave: false 
+    })) 
     // 4-2. request body parser
     .use(express.urlencoded({extended: true}))  // application/x-www-form-urlencoded
     .use(express.json())                        // application/json
-    // 4-3. view engine setup
+    // 4-3. Multipart
+    .use(multer({
+        dest: path.join(__dirname, process.env.MULTER_TEMPORARY_STORE)
+    }).single('file'))
+    // 4-4. static resources
+    .use(express.static(path.join(__dirname, process.env.STATIC_RESOURCES_DIRECTORY)))
+    // 4-5. view engine setup
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'ejs');
 
--- mysite/models/index.js
+++ mysite/models/index.js
@@ -2,18 +2,45 @@ const {Sequelize} = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
+    port: process.env_DB_PORT,
     dialect: 'mysql'
 });
 
-// Import Mappling Object
+// Import Mapping Object
 const User = require('./User')(sequelize);
+const Guestbook = require('./Guestbook')(sequelize);
+const Gallery = require('./Gallery')(sequelize);
+const Board = require('./Board')(sequelize);
+
+User.hasMany(Board, {
+    foreignKey: {
+        name: 'userNo',
+        allowNull: false,
+        constraints: true,
+        onDelete: 'CASECADE' 
+    }
+});
 
 // DB에 반영(DDL)
 User.sync({
-    force: process.env.TABLE_CREATE_ALWAYS ==='true',
-    alter: process.env.TABLE_ALTER_ALWAYS ==='true'
+    force: process.env.TABLE_CREATE_ALWAYS === 'true',
+    alter: process.env.TABLE_ALTER_ALWAYS === 'true'
+});
+
+Guestbook.sync({
+    force: process.env.TABLE_CREATE_ALWAYS === 'true',
+    alter: process.env.TABLE_ALTER_ALWAYS === 'true'
+});
+
+Gallery.sync({
+    force: process.env.TABLE_CREATE_ALWAYS === 'true',
+    alter: process.env.TABLE_ALTER_ALWAYS === 'true'
+});
+
+Board.sync({
+    force: process.env.TABLE_CREATE_ALWAYS === 'true',
+    alter: process.env.TABLE_ALTER_ALWAYS === 'true'
 });
 
 // Export Mapping Object
-module.exports = {User};
+module.exports = {User, Guestbook, Gallery, Board};
